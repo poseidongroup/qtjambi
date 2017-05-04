3c7ede6aaeeb4df1311255af550a42adebf89c96
Raymond Dahlberg <rd@poseidon.no>
2017-05-04 09:47:43 +0200

    Added batch file for setting paths needed for building Jambi. Need to be configured before calling ant all

---
+++ set-build-env-paths.bat
@@ -0,0 +1,6 @@
+#Set up Jambi build environment
+set JAVA_HOME=C:\Program Files (x86)\Java\jdk1.7.0_17
+set QTDIR=C:\Qt\4.8.4-vs2010
+set ANTDIR=C:\API\apache-ant-1.8.4
+set JAMBIDIR=C:\Qt\qtjambi-community-git-20130305
+set PATH=%QTDIR%\bin;%JAVA_HOME%\bin;%ANTDIR%\bin;%JAMBIDIR%\build\qmake-generator\release;%PATH%
